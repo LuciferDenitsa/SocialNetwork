0607279824f53fd7cef57315d358056c57c2c95b
Daniil <rychenkov16@yandex.ru>
2025-01-03 02:22:25 +0300

    add program cs

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace SocialNetwork
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
+
+namespace SocialNetwork
 {
-    internal class Program
+    class Program
     {
+        public static UserService userService = new UserService();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать в социальную сеть.");
+
+            while (true)
+            {
+                Console.WriteLine("Для регистрации пользователя введите имя пользователя:");
+
+                string firstName = Console.ReadLine();
+
+                Console.Write("фамилия:");
+                string lastName = Console.ReadLine();
+
+                Console.Write("пароль:");
+                string password = Console.ReadLine();
+
+                Console.Write("почтовый адрес:");
+                string email = Console.ReadLine();
+
+                UserRegistrationData userRegistrationData = new UserRegistrationData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userService.Register(userRegistrationData);
+                    Console.WriteLine("Регистрация произошла успешно!");
+                }
+
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Введите корректное значение.");
+                }
+
+                catch (Exception)
+                {
+                    Console.WriteLine("Произошла ошибка при регистрации.");
+                }
+
+                Console.ReadLine();
+            }
+
         }
     }
 }
